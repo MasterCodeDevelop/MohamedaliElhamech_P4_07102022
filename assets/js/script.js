3dae2b7901deada268b1a89afe9976693fbb98a6
master <master.code.develop@gmail.com>
2022-10-11 18:15:35 +0200

    verify form name

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@
 const modalbg = document.querySelector(".bground"),
 modalBtn = document.querySelectorAll(".modal-btn"),
 formData = document.querySelectorAll(".formData"),
-close = document.querySelector(".close");
+close = document.querySelector(".close"),
+submit = document.querySelector(".btn-submit"),
+first = document.getElementById("first");
+
+// Regex
+const RegName = /^[a-zA-Z\- ]{2,20}$/i;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -17,6 +22,23 @@ close.addEventListener("click", () => {
   modalbg.style.display = "none";
 })
 
+// if the  submitted form
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+})
+
+// verify FirstName
+first.addEventListener("change", () => verifyFirstName());
+const verifyFirstName = () => {
+  let { value, parentElement } = first,
+  length = value.length - (value.split(" ").length-1);
+
+  if (length == 0) displayDataError(parentElement, "Veuillez entrer votre prénom.");
+  else if (length < 2) displayDataError(parentElement, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+  else if (!RegName.test(value)) displayDataError(parentElement, "Veuillez entrer un prénom correct.");
+  else return removeDataErrror(parentElement);
+}
+
 // allows you to change the status of topnav
 function editNav() {
   var x = document.getElementById("myTopnav");
@@ -25,4 +47,19 @@ function editNav() {
   } else {
     x.className = "topnav";
   }
+}
+
+// displays the error message
+function displayDataError(parentElement, message) {
+  parentElement.setAttribute("data-error",message);
+  parentElement.setAttribute("data-error-visible", "true");
+}
+
+// removes the error message
+function removeDataErrror(parentElement) {
+  if (parentElement.getAttribute("data-error") || parentElement.getAttribute("data-error-visible")) {
+    parentElement.removeAttribute("data-error");
+    parentElement.removeAttribute("data-error-visible");
+  }
+  return true;
 }
