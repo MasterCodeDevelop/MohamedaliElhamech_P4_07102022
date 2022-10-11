b26b3cc9026d7202162eed94db9784b8fa086b5d
master <master.code.develop@gmail.com>
2022-10-11 18:02:15 +0200

    modal closes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground"),
+modalBtn = document.querySelectorAll(".modal-btn"),
+formData = document.querySelectorAll(".formData"),
+close = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +12,17 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// closes the modal
+close.addEventListener("click", () => {
+  modalbg.style.display = "none";
+})
 
+// allows you to change the status of topnav
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
